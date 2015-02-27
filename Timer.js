6979b97a3cbb36affb643aaa0b9c520d319db134
Kristi Marks <panhead89@msn.com>
2015-02-26 16:58:32 -0800

    Adding Documentation 2
    Added remaining Documentation

--- Timer.js
+++ Timer.js
@@ -1,33 +1,61 @@
-/*Timer.js*/
+/**Timer.js: A Timer Class giving a timer's functionality using an observer type pattern**/
+/**TODO: add deleteListener() function
+***TODO: clean up old event stuff
+*/
+
 //var CurrentTimer;
-	Timer=function(intrval,id)
-	{
+
+/**A global Array that holds all timers in the project**/
+var timers=[];
+/**
+*A Constructor for a timer.
+*@param intrval{integer} the interval,in milliseconds, with which you want the timers event and listeners to fire
+*@param id{integer} the index of the timer in the global timers array
+**/
+Timer=function(intrval,id)
+{
+/**This Attribute is the "current time" of the timer it basically keeps count of how many times increment has fired  **/
 		this.currentTime=0;
+/**This Attribute is the interval, in milliseconds, on which the functions fire **/
 		this.intervalTime=intrval;
-this.incrementEvent=new CustomEvent('increment',{detail:{timer:this}});
+/**This Attribute is the Event that is listened for in the Observer Pattern**/		
+		this.incrementEvent=new CustomEvent('increment',{detail:{timer:this}});
 //document.addEventListener('increment',this.increment,false)*/
-		this.listeners=[]
+/**This Attribute is the array of listeners that are called every time the timer is incremented**/
+		this.listeners=[];
 		this.ID=id;
-	this.start=function()
+		
+/**
+*This function starts the Timer.
+**/		
+this.start=function()
 	{
 	
 	//CurrentTimer=this;
-		this.currentTime=0;
+		//this.currentTime=0;
 //console.log("Time started:"+this.currentTime);
 		this.interval=setInterval(function(){ 
 		var time=timers[id]; 
 		time.dispatchIncrement(time.incrementEvent)},
 		this.intervalTime);
 	}
-	Timer.prototype.increment=function(e)
+/**
+*This function increments the Timer's currentTime.
+*@param e{Event} The event used for the Observer type pattern.
+**/	
+Timer.prototype.increment=function(e)
 	{
 		var timer=e.detail.timer;
 		timer.currentTime++;
-console.log("ID: "+timer.ID+" currTime: "+ timer.currentTime);
+//console.log("ID: "+timer.ID+" currTime: "+ timer.currentTime);
 	}
 
-	Timer.prototype.dispatchIncrement=function(event)
-	{
+/**
+*This function is responsible for calling all of the functions that are listening to the Timed Events.
+*@param event{Event} The event used for the Observer type pattern.
+**/	
+Timer.prototype.dispatchIncrement=function(event)
+{
 //document.dispatchEvent(event);
 //console.log("dispatch inc");
 	this.increment(event);
@@ -35,19 +63,30 @@ console.log("ID: "+timer.ID+" currTime: "+ timer.currentTime);
 	{
 		this.listeners[i].call();
 	}
-	} 
-	Timer.prototype.stop=function()
-	{
+} 
+
+/**
+*This function stops the Timer.
+**/		
+Timer.prototype.stop=function()
+{
+		this.currentTime=0;
 		clearInterval(this.interval);
 //alert("Timestopped:");
-	
-	}
-	Timer.prototype.getTime=function()
-	{
+}
+/**
+*This function returns the currentTime of the Timer.
+*@return this.currentTime 
+**/	
+Timer.prototype.getTime=function()
+{
 		return this.currentTime;
-	}
-	Timer.prototype.toString=function()
-	{ 
+}
+/**
+*@return The String representation of this Timer object 
+**/
+Timer.prototype.toString=function()
+{ 
 		var time;
 		var minutes=Math.floor(this.currentTime/60000);
 		time=this.currentTime%60000;
@@ -56,17 +95,28 @@ console.log("ID: "+timer.ID+" currTime: "+ timer.currentTime);
 //console.log("Minutes: "+minutes+" Seconds: "+seconds+" mili: "+time);
 		return ""+minutes+":"+seconds+"."+time;
 
-	}
-	Timer.prototype.addListener=function(toadd,param)
-	{
+}
+
+/**
+*This function adds a Function object to the listeners array
+*@param toadd{function} the function that is made into a Function object and added to the Listeners Array
+*@param param{Array} the Array of parameters needed in order to call the corresponding function.
+**/
+Timer.prototype.addListener=function(toadd,param)
+{
 		if(typeof toadd==='function')
 			this.listeners.push(new Function(toadd, param));
 		else
 		{
 			alert("cannot add listener"+toadd+"because it is not a function" )
 		}
-	}
-	
+}
+
+/**
+*A constructor for a Function object. A convenience wrapper object for internal use to make keeping an array of functions and parameters easier.
+*@param fun{function} the function to be kept.
+*@param param{array} The array of parameters used to call the function.
+**/	
 Function=function(fun,param)
 {
 	if(typeof fun==='function')
@@ -74,6 +124,9 @@ Function=function(fun,param)
 	this.func=fun;
 	this.params=param;
 	}
+/**
+*Calls this function with the corresponding params.
+**/
 this.call=function()
 	{
 //console.log("calling function: "+this.func)
--- setUp.js
+++ setUp.js
@@ -1,20 +1,32 @@
-/* An Array of background colors for GUI of timers */
+/** An Array of background colors for GUI of timers **/
 var backgrounds=["darkred","darkblue", "darkgreen", "black"];
-/* An Array of foreground colors for GUI of timers */
+
+/** An Array of foreground colors for GUI of timers **/
 var foregrounds=["red","lightblue", "lightgreen", "yellow"];
-var timers=[];
+
+
+/**This function is called when the document loads**/
 $(document).ready(setUp); // End Document Ready
+
+/**
+*This function handles what happens to build the page
+**/
 function setUp()
 {
 makeTimers(4);
 
- }
+}
+
+/**
+* This function makes the specified number of timers and adds them to the global array then creates the corresponding GUI and event listeners
+*@param number{integer} The number of timers to build  	
+**/
 function makeTimers(number)
 {
 
 	for(var i=0; i<number; i++)
 	{	$("#timers").append("<div class=element id='timer_container"+(i+1)+"'></div>")
-		//console.log("making timer "+(i+1));
+//console.log("making timer "+(i+1));
 		timers[i]=new Timer(100,i);
 		var timerGui="<div class='timer' id='timer"+(i+1)+"'style='background-color:"+backgrounds[i]+"; color:"+foregrounds[i]+"'>0:00.00</div><button id='timer"+(i+1)+"startButton' Style='width:100%; height:32%;'>Start Time</button></div>";
 		$("#timer_container"+(i+1)).append(timerGui);
@@ -23,6 +35,11 @@ function makeTimers(number)
 		timers[i].addListener(updateTimer,[timers[i]])
 	}
 }
+
+/** 
+* This function handles what happens when the user clicks the stop/start button
+* @param event{Event} The onClick event that triggered this function.
+**/
 function startStopTimer(event)
 {
 	var button=event.data[0];
@@ -40,9 +57,13 @@ function startStopTimer(event)
 	}
 }
 
+/**
+*This function handles what happens to update the timer
+*@param timerArray{Array} an array of params that really just holds the timer to update.
+**/
 function updateTimer(timerArray)
 {
 	var timer=timerArray[0];
-console.log("updateTimer:"+timer)
-$("#timer"+(timer.ID+1)).text(timer);
+//console.log("updateTimer:"+timer)
+	$("#timer"+(timer.ID+1)).text(timer);
 }
